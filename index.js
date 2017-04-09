e56e43710b57fbd345298787875daa09add118ed
Marius Craciunoiu <marius@craciunoiu.net>
2017-04-08 20:35:24 -0600

    Use sync version of rate limiter

--- index.js
+++ index.js
@@ -6,20 +6,17 @@ const cache = LRU({
   max: 15000,
 })
 
-const rateLimit = (maxTokens, period) => next => async (req, res) => {
+const rateLimit = (maxTokens, period) => next => (req, res) => {
   const ip = proxyaddr(req, ['loopback', 'linklocal', 'uniquelocal'])
   const limiter = cache.get(ip) || new RateLimiter(maxTokens, period, true)
-  return new Promise((resolve, reject) => {
-    limiter.removeTokens(1, (err, remainingTokens) => {
-      cache.set(ip, limiter)
-      if (err || remainingTokens === -1) {
-        const error = new Error('Too Many Requests')
-        error.statusCode = 429
-        return reject(error)
-      }
-      return resolve(next(req, res))
-    })
-  })
+  const hasMoreTokens = limiter.tryRemoveTokens(1)
+  cache.set(ip, limiter)
+  if (hasMoreTokens === false) {
+    const error = new Error('Too Many Requests')
+    error.statusCode = 429
+    throw error
+  }
+  return next(req, res)
 }
 
 module.exports = {
